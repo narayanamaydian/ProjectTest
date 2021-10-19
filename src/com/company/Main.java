35755fe34569e966c98236b0e0e3be3c1314ed77
Narayana Maydian <narayanamaydian@gmail.com>
2021-10-19 21:07:54 +0700

    Nih gw benerin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,7 @@ package com.company;
 
 public class Main {
 
-    System.out.println("Hello World");
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World");
     }
 }
